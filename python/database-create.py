a4f772b01db9811c6c513f781debaab3afd16b3c
Christopher Isene <christopher.isene@gmail.com>
2023-03-26 18:32:35 +0200

    database-create

--- python/database-create.py
+++ python/database-create.py
@@ -2,4 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import os
-import sqlite
+import datetime
+
+import sqlite3
+from sqlite3 import Error
+
+import yaml
+
+
+def main():
+  pass
+
+
+if __name__ == '__main__':
+  main()
